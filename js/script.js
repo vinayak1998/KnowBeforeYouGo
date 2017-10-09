ba61d6d1fb82575daa775b180c116c52a7267460
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-10-10 00:55:48 +0530

    google streetview api successfully implemented

--- js/script.js
+++ js/script.js
@@ -13,15 +13,26 @@ function loadData() {
 
     // load streetview
 
-    // YOUR CODE GOES HERE!
+    // the CODE is HERE! :D
     var streetStr = $("#street").val();
     var cityStr = $("#city").val();
-    var address = streetStr+", "+cityStr;
-    $greeting.text("So you wanna live at " + address + " ?");
 
-    var streetviewUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location="+address+'';
+    //The full address
+    var address = streetStr+", "+cityStr;
+
+    //Friendly greeting text :p
+    $greeting.text("Oh nice! So you wanna live at " + address + "? Good choice! ;)");
+
+    //hardocode the size 600x 400 pixels
+    //set the location to the full address
+    var streetviewUrl = "http://maps.googleapis.com/maps/api/streetview?size=600x400&location="+address+'';
+
+    //appending the Right image to the page to the Right class with the Right url
     $body.append('<img class="bgimg" src=" ' + streetviewUrl  + ' ">');
+
+    //NY time ajax request goes here
     return false;
 };
 
+//this loads the function loadData() when someone hits "submit"
 $('#form-container').submit(loadData);
