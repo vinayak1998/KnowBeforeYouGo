bce24e9456f058ed77f2a580db56586c4878e9ad
Your NameVinayak1998git config --global user.name Your <vinayakrrrrr@gmail.com>
2017-10-10 22:19:50 +0530

    Wikipedia api implemeted

--- js/script.js
+++ js/script.js
@@ -59,7 +59,26 @@ function loadData() {
         $nytHeaderElem.text('New York Times Articles could not be loaded');
     });
 
+    // Wikipedia
+    var wikiBaseUrl = 'http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=';
+    var wikiUrl = wikiBaseUrl + city;
 
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text('Could not load wikipedia links');
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(data){
+            for (var i = 0; i <= data[1].length - 1; i++) {
+                var pageLink = '<li><a href="' + data[3][i] + '">' + data[1][i] + '</a></li>';
+                $wikiElem.append(pageLink);
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
 
 
     return false;
